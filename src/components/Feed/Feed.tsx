09a3b3b01939becff2094c72245d14bdc6d99855
DzikStar <82441193+DzikStar@users.noreply.github.com>
2025-10-04 22:56:16 +0200

    Create Feed component placeholder

--- src/components/Feed/Feed.tsx
+++ src/components/Feed/Feed.tsx
@@ -1,13 +1,20 @@
 import './Feed.css'
 
 function Feed() {
-
   return (
-    <>
     <div className='FeedContainer'>
-      FEED GOES HERE
+      <div className='FeedHeader'>
+        <h2>Feed</h2>
+      </div>
+      <div className='FeedContent'>
+        {Array.from({ length: 20 }).map((_, i) => (
+          <div key={i} className='FeedItem'>
+            <h3>Alert {i + 1}</h3>
+            <p>Description</p>
+          </div>
+        ))}
+      </div>
     </div>
-    </>
   )
 }
 
