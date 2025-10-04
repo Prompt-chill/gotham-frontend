f335814aad39ba11217d09b75c81943bf37faaf7
DzikStar <82441193+DzikStar@users.noreply.github.com>
2025-10-05 00:19:43 +0200

    Implement functional OSM map

--- src/components/MapOverlay/MapOverlay.tsx
+++ src/components/MapOverlay/MapOverlay.tsx
@@ -1,14 +1,42 @@
-import './MapOverlay.css'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import L from 'leaflet';
+import './MapOverlay.css';
+
+import iconUrl from 'leaflet/dist/images/marker-icon.png';
+import iconRetinaUrl from 'leaflet/dist/images/marker-icon-2x.png';
+import shadowUrl from 'leaflet/dist/images/marker-shadow.png';
+
+const position: L.LatLngExpression = [52.237049, 21.017532];
+
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: iconUrl,
+  iconRetinaUrl: iconRetinaUrl,
+  shadowUrl: shadowUrl,
+});
 
 function MapOverlay() {
-
   return (
     <>
-    <div className='mapOvelayContainer'>
-      MAP GOES HERE
-    </div>
+      <div className='mapOvelayContainer'>
+        <MapContainer 
+          center={position} 
+          zoom={6} 
+          style={{ height: '100%', width: '100%' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={position}>
+            <Popup>
+              Test Warszawa Halo.
+            </Popup>
+          </Marker>
+        </MapContainer>
+      </div>
     </>
-  )
+  );
 }
 
-export default MapOverlay
+export default MapOverlay;
